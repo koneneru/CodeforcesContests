59a4d29feb2d731085b8942b657730c5f1f9e07b
koneneru <koneneru@gmail.com>
2023-11-17 10:44:06 +0400

    Added Codeforces_Round_827 functions declarations

---
+++ CodeforcesContests/Codeforces_Round_827.cpp
@@ -0,0 +1,29 @@
+#include "Codeforces_Round_827.h"
+
+void Codeforces_Round_827::A()
+{
+}
+
+void Codeforces_Round_827::B()
+{
+}
+
+void Codeforces_Round_827::C()
+{
+}
+
+void Codeforces_Round_827::D()
+{
+}
+
+void Codeforces_Round_827::E()
+{
+}
+
+void Codeforces_Round_827::F()
+{
+}
+
+void Codeforces_Round_827::G()
+{
+}
--- CodeforcesContests/Codeforces_Round_827.h
+++ CodeforcesContests/Codeforces_Round_827.h
@@ -1,5 +1,12 @@
 #pragma once
 class Codeforces_Round_827
 {
+	static void A();
+	static void B();
+	static void C();
+	static void D();
+	static void E();
+	static void F();
+	static void G();
 };
 
